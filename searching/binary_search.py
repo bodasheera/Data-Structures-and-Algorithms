e29e7676881523372eb642e12bf9de1b17a1edfd
Vignesh Saikrishna Mallya <vignesh@appveen.com>
2022-06-23 18:18:26 +0530

    binary search

---
+++ searching/binary_search.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def search(self , nums: List[int], target: int) -> int:
+        begin = 0
+        end = len(nums)-1 
+
+        while(begin <= end):
+            mid = (begin + end) // 2
+            if(nums[mid] == target):
+                return mid
+            elif (nums[mid] > target):
+                end = mid - 1
+            elif (nums[mid] < target):
+                begin = mid + 1 
+
+        return -1
+
