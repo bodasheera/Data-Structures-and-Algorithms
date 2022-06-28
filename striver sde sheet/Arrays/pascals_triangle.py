5f756f96befe9b7c157eca5bb513cef582396611
Vignesh Saikrishna Mallya <vignesh@appveen.com>
2022-06-28 10:35:37 +0530

    pascals triangle optimised solution

--- striver sde sheet/Arrays/pascals_triangle.py
+++ striver sde sheet/Arrays/pascals_triangle.py
@@ -14,16 +14,31 @@ Example 2:
 
 Constraints:
     1 <= numRows <= 30
+
+
+3 questions can be asked by interviewer 
+1. print entire pascal triangle 
+2. print pascal value at nth row and cth column 
+3. print pascal row at n 
+
+
+Observations 
+1. first and last value of row is always 1
+2. each row has always columns of length of row number (1st row 1 column , 2nd row 2 columns .....)
+3. element is sum of values of 2 elements of above row
+
+
+Print entire pascal triangle 
+Time Complexity : O(N**2) N to traverse rows . N to traverse column
+Space Complexity : O(N**2) to store 2d matrix 
 """
-
-
 
 
 
 from typing import List
 
 
-class Solution:
+class SimpleSolution:
         
     def generate(self, numRows: int) -> List[List[int]]:
 
@@ -32,6 +47,7 @@ class Solution:
         if numRows >= 1:
             output.append([1])
         if numRows >= 2:
+
             output.append([1,1])
         if numRows >= 3 :
             for i in range(2,numRows):
@@ -44,7 +60,25 @@ class Solution:
                 output.append(temp)
 
         return output
+        
 
 
-test = Solution()
+class Solution2:
+        
+    def generate(self, numRows: int) -> List[List[int]]:
+
+        triangle = [0] * numRows; # pascal triagle of length n rows
+
+        for i in range(0, numRows):
+            triangle[i] = [0] * (i+1) # column length will be row number
+
+            triangle[i][0] = triangle[i][-1] = 1 # first ele and last ele will be 1
+
+            for j in range(1, i):
+                triangle[i][j] = triangle[i-1][j - 1] + triangle[i-1][j]
+
+        return triangle
+        
+
+test = Solution2()
 print(test.generate(4))
