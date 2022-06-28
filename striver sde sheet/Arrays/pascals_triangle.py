e79bd383560d39dfddd66496f8052e33aa151467
Vignesh Saikrishna Mallya <vignesh@appveen.com>
2022-06-28 17:10:40 +0530

    pascal triangle optimised approach added

--- striver sde sheet/Arrays/pascals_triangle.py
+++ striver sde sheet/Arrays/pascals_triangle.py
@@ -31,10 +31,55 @@ Observations
 Print entire pascal triangle 
 Time Complexity : O(N**2) N to traverse rows . N to traverse column
 Space Complexity : O(N**2) to store 2d matrix 
+
+--------------------------------------------
+
+Print pascal value at nth row and cth column 
+
+Binomial theoram formula (n-1)C(r-1)
+
+nCr = n!/(r!(n-r)!) 
+
+Time Complexity : O(N) - For Finding Factorial
+Space Complexity: O(1) 
+
+--------------------------------------------
+
+Print pascals row when rowNum is given 
+
+1.Use pascals formula for each element in row 
+2. row will have rowNum elements 
+
+Approach one 
+1. use binomial theoram formula and find value for each element 
+
+Time Complexity : o(N*N) -> factorial for each element and loop
+Space Complexity: O(N) -> store pascal row 
+
+Approach Two 
+2. find factorial for all elements using pattern approach in O(1) . 
+
+rowNum = 5 
+formula n-1Cr-1
+
+4C0 = 1
+4C1 = 1 * (4 / 1) # 4 is 5 -1 ie rowNum - i , 1 is i 
+4C2 = 4 * (3 / 2) = 6
+4C3 = 6 * (2 / 3) = 4 
+4C4 = 4 * (1 / 4) = 0
+
+
+formula is SUM OF ALL prev * ((rowNum - i) / i)
+
+
+Time Complexity : O(N) -> looping for all elements 
+Space Complexity: O(N) -> store pascal row 
+
 """
 
 
 
+from math import factorial
 from typing import List
 
 
@@ -63,7 +108,7 @@ class SimpleSolution:
         
 
 
-class Solution2:
+class PascalTriangleOptimised:
         
     def generate(self, numRows: int) -> List[List[int]]:
 
@@ -80,5 +125,59 @@ class Solution2:
         return triangle
         
 
-test = Solution2()
-print(test.generate(4))
+class PascalElement:
+
+    def __factorial(self, n):
+
+        if n == 1:
+            return 1
+        else:
+            return n * self.__factorial(n-1) 
+
+    def generate(self , row: int, col: int) -> int:
+        
+        n = row - 1
+        r = col - 1
+        return int(factorial(n) / (factorial(r) * factorial(n-r)))
+
+
+
+class PascalRowSimple:
+
+    def __factorial(self, n):
+
+        if n == 1:
+            return 1
+        else:
+            return n * self.__factorial(n-1) 
+
+    def generate(self , rowNum: int) -> int:
+        
+        row = [0]* rowNum 
+        row[0] = row[-1] = 1
+
+        n = rowNum - 1 
+
+        for i in range(1, rowNum -1):
+            row[i] = int(factorial(n) / (factorial(i) * factorial(n-i)))
+            
+        return row
+
+
+class PascalRowOptimized:
+
+    def generate(self ,numRows) -> List[int] :
+
+        arr = [0]*numRows # column of length row 
+
+        prev = 1
+        arr[0] = prev
+
+        for i in range(1 , numRows):
+            prev = int(prev * ((numRows-i)/i))
+            arr[i] = prev
+
+        return arr
+
+test = PascalRowOptimized()
+print(test.generate(6))
